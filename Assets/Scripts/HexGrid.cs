41c81fc81d73fe88d9bd2a012f46d838e3365f3c
ccandy <ccunandy2001@gmail.com>
2021-01-07 20:49:33 -0800

    add more files

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HexGrid : MonoBehaviour
+{
+
+    private int _height = 6;
+    private int _width = 6;
+
+    private HexCell[] _cells;
+
+    private Canvas _hexCanvas;
+    private HexMesh _hexMesh;
+
+    public HexCell HexCellPref;
+    public Text HexCellTex;
+
+
+    void Awake()
+    {
+        if(HexCellPref == null)
+        {
+            Debug.LogError("HexCell Pref is null");
+        }
+
+        _cells = new HexCell[_width * _height];
+        _hexCanvas = gameObject.GetComponentInChildren<Canvas>();
+        if(_hexCanvas == null)
+        {
+            Debug.LogError("Canvas is null");
+        }
+
+        _hexMesh = gameObject.GetComponentInChildren<HexMesh>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        for (int x = 0; x < _width; x++)
+        {
+            for (int z = 0; z < _height; z++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+
+        _hexMesh.Triangulate(_cells);
+    }
+    
+    private void CreateCell(int x, int z, int i)
+    {
+        Vector3 pos = Vector3.zero;
+        pos.x = (x + z * 0.5f - z/2)* HexMatrics.InnerRad * 2;
+        pos.y = 0;
+        pos.z = z * HexMatrics.OutterRad * 1.5f;
+
+        HexCell _cell = Instantiate<HexCell>(HexCellPref);
+        _cells[i] = _cell;
+        _cell.transform.SetParent(transform);
+        _cell.transform.localPosition = pos;
+        CreateTex(pos.x, pos.z, i);
+
+    }
+
+    private void CreateTex(float x, float z, int i)
+    {
+        Text _tex = Instantiate<Text>(HexCellTex);
+        _tex.rectTransform.SetParent(_hexCanvas.transform, false);
+        _tex.rectTransform.anchoredPosition = new Vector2(x, z);
+        _tex.text = "Cell " + i + " Pos " + x + "," + z;
+    }
+
+
+}
---
+++ Assets/Scripts/HexMesh.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+    Mesh _hexMesh;
+    List<Vector3> _vertics;
+    List<int> _triangles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hexMesh = new Mesh();
+        _vertics = new List<Vector3>();
+        _triangles = new List<int>();
+
+        MeshFilter _meshFilter = gameObject.GetComponent<MeshFilter>();
+        _meshFilter.mesh = _hexMesh;
+    }
+
+
+    public void Triangulate(HexCell[] cells)
+    {
+        _hexMesh.Clear();
+        _vertics.Clear();
+        _triangles.Clear();
+
+        for (int n = 0; n < cells.Length; n++)
+        {
+            HexCell _cell = cells[n];
+            TriangulateCell(_cell);
+        }
+        _hexMesh.vertices = _vertics.ToArray();
+        _hexMesh.triangles = _triangles.ToArray();
+        _hexMesh.RecalculateNormals();
+
+    }
+
+    private void TriangulateCell(HexCell cell)
+    {
+        Vector3 v1 = cell.transform.localPosition;
+        for(int n =0; n < 6; n++)
+        {
+            Vector3 v2 = v1 + HexMatrics.Corners[n];
+            Vector3 v3 = v1 + HexMatrics.Corners[n+1];
+
+            AddTriangle(v1, v2, v3);
+        }
+            
+    }
+
+    private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int indexCount = _vertics.Count;
+
+        _vertics.Add(v1);
+        _vertics.Add(v2);
+        _vertics.Add(v3);
+
+        _triangles.Add(indexCount);
+        _triangles.Add(indexCount + 1);
+        _triangles.Add(indexCount + 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
